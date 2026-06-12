0f84d343cbad77f754ec06b4e705b82965829072
Muhammad Sufyan Jura <sufyanfaizan47@gmail.com>
2026-06-12 21:41:37 +0500

    Refactor robots function to use object for rules

--- app/robots.js
+++ app/robots.js
@@ -2,13 +2,11 @@ const siteUrl = "https://www.pindi-ki-awaaz.app";
 
 export default function robots() {
   return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-      },
-    ],
-    sitemap: `${siteUrl}/sitemap.xml`,
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
     host: siteUrl,
+    sitemap: `${siteUrl}/sitemap.xml`,
   };
 }
